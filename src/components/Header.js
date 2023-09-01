de7e3676919960e24edb210b35d78c4b0798af86
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-09-01 14:19:47 -0300

    added margin bottom for the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 function Header() {
 	return (
-		<header>
+		<header class="mb-5">
 			<div class="text-3xl uppercase font-extrabold ">
 				<span class="text-blue-950">O que você </span>
 				<span class="text-teal-500">está procurando?</span>
