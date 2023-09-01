511fb25518be1a5b754b8e221902356e21ada301
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-09-01 14:19:29 -0300

    created searchbar component and added to app

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import SearchBar from "./Main/SearchBar";
+
+function Main() {
+	return (
+		<main class="grid grid-cols-5">
+			<SearchBar />
+		</main>
+	);
+}
+
+export default Main;
---
+++ src/components/Main/SearchBar.js
@@ -0,0 +1,12 @@
+function SearchBar() {
+	return (
+		<div class="w-full border-b-2 border-b-teal-500 border-solid">
+			<input
+				class="w-full text-teal-500 placeholder-teal-500"
+				placeholder="BUSQUE AQUI "
+			></input>
+		</div>
+	);
+}
+
+export default SearchBar;
