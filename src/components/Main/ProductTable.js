e9d1fdbeff9511799d6ccbc47130474dbe437ac0
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-09-02 13:41:31 -0300

    made filters more scalable

--- src/components/Main/ProductTable.js
+++ src/components/Main/ProductTable.js
@@ -8,50 +8,55 @@ function ProductTable(props) {
 	const [filters, setFilters] = useState([]);
 
 	useEffect(() => {
-		handleSearch();
-	}, [props]);
+		handleProducts();
+	}, [props, filters]);
 
-	const handleSearch = () => {
-		const searchText = props.searchText.toLowerCase();
+	const handleProducts = () => {
+		let items = [];
 
-		if (!!searchText) {
-			const searchResults = products.filter((item) => {
-				const itemNames = item.name.toLowerCase();
-				return itemNames.includes(searchText);
-			});
-
-			setProducts(searchResults);
-			return;
-		}
-
-		getProductsByFilter();
+		items = handleFilters();
+		items = handleSearch(items);
+		setProducts(items);
 	};
 
-	const handleFilters = (selectedFilters) => {
-		if (!selectedFilters) return;
-		setFilters(selectedFilters);
+	const handleFilters = () => {
+		if (!filters) return;
+		setFilters(filters);
 
-		getProductsByFilter(selectedFilters);
-	};
-
-	const getProductsByFilter = (selectedFilters) => {
-		const filtersToFilter = selectedFilters || filters;
-		if (filtersToFilter.length <= 0) {
-			setProducts(getProducts());
-			return;
+		if (filters.length <= 0) {
+			return getProducts();
 		}
 
 		let filteredProducts = [];
-		filtersToFilter.forEach((element) => {
+		filters.forEach((element) => {
 			const categoryIdProducts = getFilters()[element].products;
 			filteredProducts.push(...categoryIdProducts);
 		});
-		setProducts(filteredProducts);
+
+		return filteredProducts;
+	};
+
+	const handleSearch = (items) => {
+		const searchText = props.searchText.toLowerCase();
+
+		if (searchText) {
+			const searchResults = items.filter((item) => {
+				const itemNames = item.name.toLowerCase();
+				return itemNames.includes(searchText);
+			});
+			return searchResults;
+		}
+
+		return items;
+	};
+
+	const setNewFilters = (selectedFilters) => {
+		setFilters([...selectedFilters]);
 	};
 
 	return (
 		<div className="grid-cols-8 row-start-2 col-span-full grid gap-4">
-			<Filters categories={categories} handleFilters={handleFilters} />
+			<Filters categories={categories} handleFilters={setNewFilters} />
 			<Table products={products} />
 		</div>
 	);
