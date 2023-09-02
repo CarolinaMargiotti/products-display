1655e74f1a2bc525191e695fd353f2a02b7e647a
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-09-02 15:20:17 -0300

    coded basic responsive hide and show button for filters

--- src/components/Main/Filters.js
+++ src/components/Main/Filters.js
@@ -3,6 +3,7 @@ import Checkbox from "./Checkbox";
 
 function Filters(props) {
 	const [checkedFilters, setCheckedFilters] = useState([]);
+	const [showFilter, setShowFilter] = useState(true);
 
 	const categories = props.categories;
 
@@ -21,20 +22,45 @@ function Filters(props) {
 	};
 
 	return (
-		<div className="text-blue-950 flex flex-col">
-			<span>Filtros</span>
-			<div className="border-b-2 border-blue-950 mt-2"></div>
-			<form className="mt-2 grid gap-2">
-				{categories.map((category) => (
-					<Checkbox
-						key={category.id}
-						id={category.id}
-						value={category.id}
-						text={`${category.name} (${category.quantity})`}
-						handleChange={handleChecked}
-					/>
-				))}
-			</form>
+		<div>
+			{!showFilter && (
+				<button
+					className="text-teal-500 border-2 border-teal-500 rounded leading-none pb-1 px-2"
+					onClick={(e) => setShowFilter(true)}
+				>
+					&raquo;
+				</button>
+			)}
+			{showFilter && (
+				<div className="responsiveFilter p-5 pb-16 transition-all">
+					<div className="text-blue-950 flex flex-col items-center lg:items-start">
+						<div className="flex justify-between w-full">
+							<span>Filtros</span>
+							<button
+								className="text-teal-500 border-2 border-teal-500 rounded leading-none pb-1 px-2"
+								onClick={(e) => setShowFilter(false)}
+							>
+								&laquo;
+							</button>
+						</div>
+						<div
+							className="border-b-2 border-blue-950 mt-2 mb-2 lg:mb-0"
+							style={{ height: "2px", width: "100%" }}
+						></div>
+						<form className="mt-2 grid gap-2">
+							{categories.map((category) => (
+								<Checkbox
+									key={category.id}
+									id={category.id}
+									value={category.id}
+									text={`${category.name} (${category.quantity})`}
+									handleChange={handleChecked}
+								/>
+							))}
+						</form>
+					</div>
+				</div>
+			)}
 		</div>
 	);
 }
