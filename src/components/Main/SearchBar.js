e07d55315e02135cf768cbae039c749056df4de7
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-09-02 13:48:38 -0300

    added possibility for enter to search text as well

--- src/components/Main/SearchBar.js
+++ src/components/Main/SearchBar.js
@@ -4,14 +4,21 @@ import SearchIcon from "./SearchIcon";
 function SearchBar(props) {
 	const [searchText, setSearchText] = useState("");
 
+	const keyDownVerification = (event) => {
+		if (event.keyCode === 13) {
+			props.handleSearch(searchText);
+		}
+	};
+
 	return (
 		<div className="w-full border-b-2 border-b-teal-500 border-solid flex transition-all hover:pb-1 ">
 			<input
 				data-testid="searchInput"
 				value={searchText}
-				onChange={(e) => setSearchText(e.target.value)}
 				className="w-full text-teal-500 placeholder-teal-500 outline-none focus:outline-none"
 				placeholder="BUSQUE AQUI "
+				onChange={(e) => setSearchText(e.target.value)}
+				onKeyDown={(e) => keyDownVerification(e)}
 			></input>
 			<button
 				data-testid="searchButton"
