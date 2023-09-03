ec6ae79b25f822299f73faee9b18f58d56b8f926
Carolina Margiotti <55335180+CarolinaMargiotti@users.noreply.github.com>
2023-09-03 15:22:52 -0300

    added hover transition for filter button

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
 	content: ["./src/**/*.{html,js}"],
 	theme: {
-		extend: {},
+		extend: {
+			boxShadow: {
+				insetTeal: "inset 0rem 0rem 0.5px 3px rgb(20, 184, 166, 0.4);",
+			},
+		},
 	},
 	plugins: [],
 };
